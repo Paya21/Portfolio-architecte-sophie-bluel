34b7bd1d137781ab1c63b727b7ff033e5f963678
Clement Paya <clementpaya21@gmail.com>
2023-01-27 13:58:14 +0100

    Ajout filtrage

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -1,33 +1,37 @@
 let listCategory;
+let listWork;
 
-//recup de tous les travaux
-(async function fetchWorks () {
+(async function listing() {
+    //appel Api recup liste travauxj
     await fetch ('http://localhost:5678/api/works')
-        .then(works => works.json())
-        .then(async (data) => {
-            genererTravaux(data);
-    })
-    .catch(error => console.error(error));
-})();
-
-//recup des catégories de chaque travaux
-(async function fetchCat () {
-    await fetch ('http://localhost:5678/api/works')
-        .then(works => works.json())
-        .then(async (data) => {
-            listCategory = data.map(data => data.category);
-            buttonFactory(data);
-    })
-    .catch(error => console.error(error));
-})();
-
-//recup des catégories définies (3 catégories en tout)
-(async function fetchWorksCategory() {
-    await fetch ('http://localhost:5678/api/categories')
-        .then(cat => cat.json())
-        .then(data => console.log(data))
+        .then(reponse => reponse.json())
+        //tranfert de la reponse dans la variable global
+        .then(data => {
+            listWork = data; 
+        })
+        //apppel et transfer de la liste des catégories
+        .then(async(cat) => {
+            await fetch ('http://localhost:5678/api/categories')
+        .then(reponse1 => reponse1.json())
+        .then(data1 => {
+            listCategory = data1;
+            buttonFactory(listCategory, listWork);
+        })
         .catch(error => console.log(error));
+        })
+        .catch(error => console.log(error));  
+        
+        //appel fonction test et génération des travaux
+        afficheTest(listWork, listCategory);
+        genererTravaux(listWork);        
 })();
+
+
+
+function afficheTest(work, cat){
+    console.log(work);
+    console.log(cat);
+}
 
 function genererTravaux(travaux){
     
@@ -53,20 +57,53 @@ function genererTravaux(travaux){
     }
 }
 
-function buttonFactory(listeCategories) {
+function buttonFactory(listeCategories, listeTravaux) {
+
+    console.log(listeCategories);
+    console.log(listeTravaux);
     
     //création du bouton 'tous'
     const conteneurBtns = document.querySelector('#filtres');
     const btnTous = document.createElement('button');
+    btnTous.id = "resetButton";
     btnTous.innerText = "Tous";
     conteneurBtns.appendChild(btnTous);
 
-    const catId = new Set(listCategory.map(idCategorie => idCategorie.name));
-    console.log(catId);
+    conteneurBtns.addEventListener("click", (e) => {
 
+        console.log("yo ma biche !");
+        const idButton = e.target.id;
+        console.log(idButton);
+
+        if (idButton === "1") {
+            console.log("Bon ça va les log un peu ??!")
+            const listeObjets = listeTravaux.filter(objet => objet.categoryId === 1);
+            console.log(listeObjets);
+            document.querySelector('.gallery').innerHTML = "";
+            genererTravaux(listeObjets);
+        } else if (idButton === "2") {
+            const listeAppart = listeTravaux.filter(appart => appart.categoryId === 2);
+            document.querySelector('.gallery').innerHTML = "";
+            genererTravaux(listeAppart);
+        } else if (idButton === "3"){
+            const listeHotels = listeTravaux.filter(hotel => hotel.categoryId === 3);
+            console.log(listeHotels);
+            document.querySelector('.gallery').innerHTML = "";
+            genererTravaux(listeHotels);
+        } else if (idButton === "resetButton"){
+            document.querySelector('.gallery').innerHTML = "";
+            genererTravaux(listeTravaux);
+        }
+    });
+    
+    for (i = 0; i <= listeCategories.length; i++){
+
+        const btnCat = document.createElement('button');
+        btnCat.innerText = listeCategories[i].name;
+        btnCat.id = listeCategories[i].id;
+        conteneurBtns.appendChild(btnCat);
+    }    
 }
-
-
 
 
 
